e343cd96fd06293d2046ecddd2b3541331f19081
yorung <yorung@gmail.com>
2014-07-27 20:27:56 +0900

    a bat file to copy required files from angle project filders

---
+++ hello_triangle/copy_files.bat
@@ -0,0 +1,4 @@
+copy ..\angle\projects\build\Release_Win32\d3dcompiler_46.dll Release_Win32\
+copy ..\angle\projects\build\Release_Win32\*.tga Release_Win32\
+copy ..\angle\projects\build\Debug_Win32\d3dcompiler_46.dll Debug_Win32\
+copy ..\angle\projects\build\Debug_Win32\*.tga Debug_Win32\
