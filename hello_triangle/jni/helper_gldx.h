8f942a6c47b4c1222fb16b297edba7e1065dc0db
yorung <yorung@gmail.com>
2015-01-30 23:22:15 +0900

    merge helper from WGLGrabber project

---
+++ hello_triangle/jni/helper_gldx.h
@@ -0,0 +1,35 @@
+typedef unsigned short AFIndex;
+
+#ifdef GL_TRUE
+#define AFIndexTypeToDevice GL_UNSIGNED_SHORT
+typedef GLuint AFBufObj;
+#endif
+
+#ifdef __d3d11_h__
+#define AFIndexTypeToDevice DXGI_FORMAT_R16_UINT
+typedef ID3D11Buffer* AFBufObj;
+#endif
+
+#ifdef GL_TRUE
+inline void afSafeDeleteBuffer(GLuint& b)
+{
+	if (b != 0) {
+		glDeleteBuffers(1, &b);
+		b = 0;
+	}
+}
+#endif
+
+#ifdef __d3d11_h__
+inline void afSafeDeleteBuffer(AFBufObj& b)
+{
+	SAFE_RELEASE(b);
+}
+#endif
+
+AFBufObj afCreateIndexBuffer(const AFIndex* indi, int numIndi);
+AFBufObj afCreateQuadListIndexBuffer(int numQuads);
+AFBufObj afCreateVertexBuffer(int size, const void* buf);
+AFBufObj afCreateDynamicVertexBuffer(int size);
+void afWriteBuffer(AFBufObj bo, const void* buf, int size);
+void afDrawIndexedTriangleList(AFBufObj ibo, int count, int start = 0);
