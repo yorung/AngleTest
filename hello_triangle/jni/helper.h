8decfd3fd146cc2564fb853b051b1a2f334c6c92
yorung <yorung@gmail.com>
2015-01-30 22:38:49 +0900

    merge helper function

--- hello_triangle/jni/helper.h
+++ hello_triangle/jni/helper.h
@@ -1,6 +1,7 @@
 #define dimof(x) (sizeof(x) / sizeof(x[0]))
 
 void *LoadFile(const char *fileName, int* size = nullptr);
+bool SaveFile(const char *fileName, const uint8_t* buf, int size);
 double GetTime();
 float Random();
 void GoMyDir();
